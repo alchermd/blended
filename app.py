41af19bc77e592775840185f1fc4dc33e1f85553
alchermd <johnalcherdoloiras@gmail.com>
2017-08-17 22:18:44 +0800

    Initial commit.

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import pytesseract
+import glob
+
+def main():
+    """
+    Reads the contents of the images folder and writes them into
+    a file named output.txt
+
+    TODO: Automate the population of images folder.
+    """
+    try:
+        import Image
+    except ImportError:
+        from PIL import Image
+
+    for pg, img in enumerate(sorted(glob.glob('./images/*'))):
+        with open('output.txt', 'a+') as f:
+            f.write(
+                '\n --- THIS IS PAGE #{} --- \n'.format(pg + 1) +
+                pytesseract.image_to_string(Image.open(img))
+            )
+
+
+if __name__ == '__main__':
+    main()
